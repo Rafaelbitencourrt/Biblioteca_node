d5c7248c2b41ecb40f9ff2205fdaf77d626f7a46
Rafael <rafaelbitencourrt@gmail.com>
2022-04-20 23:08:50 -0300

    update

--- index.js
+++ index.js
@@ -5,7 +5,25 @@ function trataErro(erro) {
   throw new Error(chalk.red(erro.code));
 }
 
-function pegaArquivo(caminhoDoArquivo) {
+async function pegaArquivo(caminhoDoArquivo) {
+  const encoding = "utf-8";
+  try {
+    const texto = await fs.promises.readFile(caminhoDoArquivo, encoding);
+    console.log(chalk.green(texto));
+  } catch (erro) {
+    trataErro(erro);
+  }
+}
+
+/* function pegaArquivo(caminhoDoArquivo) {
+  const encoding = "utf-8";
+  fs.promises
+    .readFile(caminhoDoArquivo, encoding)
+    .then((texto) => console.log(texto))
+    .catch((erro) => trataErro(erro));
+} */
+
+/* function pegaArquivo(caminhoDoArquivo) {
   const encoding = "utf-8";
   fs.readFile(caminhoDoArquivo, encoding, (erro, data) => {
     if (erro) {
@@ -14,6 +32,6 @@ function pegaArquivo(caminhoDoArquivo) {
       console.log(chalk.green(data));
     }
   });
-}
+} */
 
 pegaArquivo("./arquivos/texto1.md");
