27e270b345a54ed3161aa968a11d6b50b5c73fa7
Rafael <rafaelbitencourrt@gmail.com>
2022-04-22 17:14:18 -0300

    update

--- cli.js
+++ cli.js
@@ -1,11 +1,16 @@
 import pegarArquivo from "./index.js";
 import chalk from "chalk";
+import validaURLs from "./http-validacao.js";
 
 const caminho = process.argv;
 
 async function processaTexto(caminhoDeArquivo) {
   const resultado = await pegarArquivo(caminhoDeArquivo[2]);
-  console.log(chalk.yellow("Lista de links"), resultado);
+  if (caminho[3] === "validar") {
+    console.log(chalk.yellow("Links Validados")), validaURLs(resultado);
+  } else {
+    console.log(chalk.yellow("Lista de links"), resultado);
+  }
 }
 
 processaTexto(caminho);
