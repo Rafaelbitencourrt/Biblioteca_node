cb238ba4c7c603a1e1e0abb037618c50c0b81c91
Rafael <rafaelbitencourrt@gmail.com>
2022-04-22 13:36:11 -0300

    update

--- cli.js
+++ cli.js
@@ -1,4 +1,11 @@
-import getFile from "./index.js";
+import pegarArquivo from "./index.js";
+import chalk from "chalk";
 
 const caminho = process.argv;
-console.log(getFile(caminho[2]));
+
+async function processaTexto(caminhoDeArquivo) {
+  const resultado = await pegarArquivo(caminhoDeArquivo[2]);
+  console.log(chalk.yellow("Lista de links"), resultado);
+}
+
+processaTexto(caminho);
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import chalk from "chalk";
 import fs from "node:fs";
+import path from "node:path";
 
 const texto =
   "São geralmente recuperados a partir de um objeto [FileList](https://developer.mozilla.org/pt-BR/docs/Web/API/FileList) que é retornado como resultado da seleção, pelo usuário, de arquivos através do elemento[<input>](https://developer.mozilla.org/pt-BR/docs/html/Element/Input) , a partir do objeto [DataTransfer](https://developer.mozilla.org/pt-BR/docs/Web/API/DataTransfer) utilizado em operações de arrastar e soltar, ou a partir da API mozGetAsFile() em um [HTMLCanvasElement](https://developer.mozilla.org/pt-BR/docs/Web/API/HTMLCanvasElement). Em Gecko, códigos com privilégiios podem criar objetos File representando qualquer arquivo local sem a intereção do usuário (veja [Implementation notes](https://developer.mozilla.org/pt-BR/docs/Web/API/File#implementation_notes) para mais informações.)";
@@ -13,23 +14,32 @@ function extraiLinks(texto) {
   while ((temp = regex.exec(texto)) != null) {
     arrayResultados.push({ [temp[1]]: temp[2] });
   }
-  return arrayResultados;
+  return arrayResultados.length === 0 ? "Não há links" : arrayResultados;
 }
 
 function trataErro(erro) {
   throw new Error(chalk.red(erro.code));
 }
 
-export default async function getFile(pathOfFile) {
+async function pegarArquivo(caminho) {
+  const caminhoAbsoluto = path.join("__dirname", "..", caminho);
   const encoding = "utf-8";
   try {
-    const text = await fs.promises.readFile(pathOfFile, encoding);
-    console.log(extractLinks(text));
+    const arquivos = await fs.promises.readdir(caminhoAbsoluto, { encoding });
+    const result = await Promise.all(
+      arquivos.map(async (arquivo) => {
+        const localArquivo = `${caminhoAbsoluto}/${arquivo}`;
+        const texto = await fs.promises.readFile(localArquivo, encoding);
+        return extraiLinks(texto);
+      })
+    );
+    return result;
   } catch (erro) {
-    trataErro(erro);
+    return trataErro(erro);
   }
 }
 
+export default pegarArquivo;
 //pegaArquivo("./arquivos/texto1.md");
 
 /* function pegaArquivo(caminhoDoArquivo) {
