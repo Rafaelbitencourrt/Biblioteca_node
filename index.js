bc6856d3ad8819db4ed15c66205efa05d042bb7b
Rafael <rafaelbitencourrt@gmail.com>
2022-04-27 11:54:18 -0300

    update

--- index.js
+++ index.js
@@ -1,28 +1,28 @@
-const chalk = require('chalk');
-const fs = require('fs');
+import chalk from "chalk";
+import fs from "node-fs";
 
 function extraiLinks(texto) {
   const regex = /\[([^\]]*)\]\((https?:\/\/[^$#\s].[^\s]*)\)/gm;
   const arrayResultados = [];
   let temp;
-  while((temp = regex.exec(texto)) !== null) {
-    arrayResultados.push({ [temp[1]]: temp[2] })
+  while ((temp = regex.exec(texto)) !== null) {
+    arrayResultados.push({ [temp[1]]: temp[2] });
   }
-  return arrayResultados.length === 0 ? 'não há links' : arrayResultados;
+  return arrayResultados.length === 0 ? "não há links" : arrayResultados;
 }
 
 function trataErro(erro) {
-  throw new Error(chalk.red(erro.code, 'não há arquivo no caminho'));
+  throw new Error(chalk.red(erro.code, "não há arquivo no caminho"));
 }
 
 async function pegaArquivo(caminhoDoArquivo) {
-  const encoding = 'utf-8';
+  const encoding = "utf-8";
   try {
-    const texto = await fs.promises.readFile(caminhoDoArquivo, encoding)
+    const texto = await fs.promises.readFile(caminhoDoArquivo, encoding);
     return extraiLinks(texto);
-  } catch(erro) {
+  } catch (erro) {
     trataErro(erro);
   }
 }
 
-module.exports = pegaArquivo;
+export default pegaArquivo;
