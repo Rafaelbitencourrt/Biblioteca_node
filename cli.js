20860825d17da691aa458cb5e070a4921068bba7
Rafael <rafaelbitencourrt@gmail.com>
2022-04-27 12:26:08 -0300

    update

--- cli.js
+++ cli.js
@@ -1,17 +1,15 @@
-#!/usr/bin/env node
-
-const chalk = require('chalk');
-const pegaArquivo = require('./index');
-const validaURLs = require('./http-validacao');
+import chalk from "chalk";
+import pegaArquivo from "./index";
+import validaURLs from "./http-validacao";
 
 const caminho = process.argv;
 
 async function processaTexto(caminhoDeArquivo) {
   const resultado = await pegaArquivo(caminhoDeArquivo[2]);
-  if (caminho[3] === 'validar') {
-    console.log(chalk.yellow('links validados'), await validaURLs(resultado));
+  if (caminho[3] === "validar") {
+    console.log(chalk.yellow("links validados"), await validaURLs(resultado));
   } else {
-    console.log(chalk.yellow('lista de links'), resultado);
+    console.log(chalk.yellow("lista de links"), resultado);
   }
 }
 
